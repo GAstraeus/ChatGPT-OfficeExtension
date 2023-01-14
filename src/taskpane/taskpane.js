c8ddb05770c954126c2fd9809430c195706d16e7
George <gdiwan123@gmail.com>
2023-01-14 12:28:39 -0500

    Add proper error handeling to show customers clear error messages and comply with Microsoft Standards

--- src/taskpane/taskpane.js
+++ src/taskpane/taskpane.js
@@ -7,16 +7,13 @@ var API_KEY = 'ENTER_API_KEY_HERE';
 
   async function sendMessageToChatGPT(message) {
     try {
-      // Set the prompt for GPT-3 to respond to
       let token_count =  message.split(" ").length;
-      // Set the parameters for the GPT-3 request
       const params = {
         prompt: message,
         model: 'text-davinci-003',
         temperature: 0.5,
         max_tokens: 4000-token_count-1 // Change this to say 4000-token_count-1 during demos
       };
-      // Call the GPT-3 API
       console.log(`Sending Message`);
       const response = await axios.post('https://api.openai.com/v1/completions', params, {
         headers: {
@@ -24,21 +21,51 @@ var API_KEY = 'ENTER_API_KEY_HERE';
           'Authorization': `Bearer ${API_KEY}`
         }
       });
-      console.log(response.data.choices[0].text);
-      return await response;
-    } catch (error) {
-      console.error(error);
-      return null;
+    if (response.status === 200) {
+      // The request was successful
+      return [response.data.choices[0].text, 200];
+    } else {
+      throw new Error(`HTTP error, status = ${response.status}`);
+    }
+  } catch (error) {
+    if (error.response) {
+      if (error.response.status === 400) {
+        console.log("Error: The request was invalid or malformed. Try Again");
+        return ["Error: The request was invalid or malformed. Try Again", 400];
+      }
+      else if (error.response.status === 401) {
+        console.log("Error: Invalid API key or not authorized to access the API");
+        return ["Error: Invalid API key or not authorized to access the API", 401];
+      } 
+      else if (error.response.status === 403) {
+        console.log("Error: The API key does not have the necessary permissions for the request. Email: dennis.cherian@hellotinker.com for a new access key");
+        return ["Error: The API key does not have the necessary permissions for the request. Email: dennis.cherian@hellotinker.com for a new access key", 403];
+      }
+      else if (error.response.status === 429) {
+        console.log("Error: The user has sent too many requests in a given amount of time. Please wait a minute and try again");
+        return ["Error: The user has sent too many requests in a given amount of time. Please wait a minute and try again", 429];
+      }
+      else if (error.response.status === 500) {
+        console.log("Error: The server failed to generate a reply");
+        return ["Error: The server failed to generate a reply", 500];
+      }
+      else {
+        console.log("Error: " + error.message);
+        return ["Error: The server failed to generate a reply", 500];
+      }
+    } else {
+      console.log("Error: " + error.message);
+      return ["Error: The server failed to generate a reply", 500];
     }
   }
+}
+
 
   export async function getMessageFromChatGPT(message) {
-    const response = await sendMessageToChatGPT(message);
-    if (response == null) {
-      console.log("Error calling chat gpt");
-      return "";
-    }
-    return response.data.choices[0].text;
+      const [response, status] = await sendMessageToChatGPT(message);
+      console.log("Got formatted server response");
+      console.log(response, status);
+      return [response, status];
   }
 
 Office.onReady((info) => {
@@ -49,18 +76,26 @@ Office.onReady((info) => {
 
 export async function submitHandler(event) {
   return Word.run(async (context) => {
+    document.getElementById("error_pane").innerText = "";
     document.getElementById("submit-chat").disabled = true;
     document.getElementById("loading-animation").style.visibility = 'visible';
     API_KEY = document.getElementById("key-for-chat-gpt").value;
     var message = document.getElementById("text-input-for-chat-gpt").value;
-    let reply = await getMessageFromChatGPT(message);
-
-    let reply_lines = reply.split("\n")
-    for (let i = 0; i < reply_lines.length; i++) {
-      context.document.body.insertParagraph(reply_lines[i], Word.InsertLocation.end);
+    let [reply, status] = await getMessageFromChatGPT(message);
+    console.log("In main again");
+    if (status === 200){
+      let reply_lines = reply.split("\n")
+      for (let i = 0; i < reply_lines.length; i++) {
+        context.document.body.insertParagraph(reply_lines[i], Word.InsertLocation.end);
+      }
+      await context.sync();
+      console.log("Working");
+    }
+    else {
+      console.log("Post error");
+      document.getElementById("error_pane").innerText = reply;
     }
 
-    await context.sync();
     document.getElementById("loading-animation").style.visibility = 'hidden';
     document.getElementById("submit-chat").disabled = false;
   });
